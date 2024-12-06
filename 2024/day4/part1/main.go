1a010b1029660b89ec938a966ae70593542f345a
Nick Allen <nick.allen@dataglide.co>
2024-12-06 20:56:18 +0000

    Solve day 4 part 1

--- 2024/day4/part1/main.go
+++ 2024/day4/part1/main.go
@@ -10,17 +10,18 @@ const (
 	x = 'X'
 )
 
-var maxGridDepth int
+var maxGridHeight int
 var maxGridWidth int
 
 func main() {
-	runeGrid, err := readFile("test.txt")
+	// runeGrid, err := readFile("test.txt")
+	runeGrid, err := readFile("../input.txt")
 	if err != nil {
 		fmt.Printf("Error reading input: %s", err.Error())
 		os.Exit(1)
 	}
 
-	maxGridDepth = len(*runeGrid)
+	maxGridHeight = len(*runeGrid)
 	maxGridWidth = len((*runeGrid)[0])
 
 	for _, runeList := range *runeGrid {
@@ -29,6 +30,13 @@ func main() {
 		}
 		fmt.Println()
 	}
+
+	fmt.Printf("max height: %d, max width: %d\n", maxGridHeight, maxGridWidth)
+
+	total := findXmasCount(runeGrid)
+
+	fmt.Println()
+	fmt.Printf("Total xmas: %d\n", total)
 }
 
 func findXmasCount(runeGrid *[][]rune) int {
@@ -39,29 +47,31 @@ func findXmasCount(runeGrid *[][]rune) int {
 		}
 	}
 
-	return 0
+	return total
 }
 
-func startSearch(gridIndex, lineIndex int, runeGrid *[][]rune) int {
-	currentRune := (*runeGrid)[gridIndex][lineIndex]
+func startSearch(yIndex, xIndex int, runeGrid *[][]rune) int {
+	currentRune := (*runeGrid)[yIndex][xIndex]
 	if currentRune != 'X' {
 		return 0
 	}
 
-	return 0
+	// fmt.Println("Found X")
+
+	return searchAround(yIndex, xIndex, runeGrid, 'M', nil)
 }
 
-func searchAround(gridIndex, lineIndex int, runeGrid *[][]rune, searchValue rune) bool {
+func searchAround(yIndex, xIndex int, runeGrid *[][]rune, searchValue rune, alreadyMovingInDirection *int) int {
 	positions := make([]Coordinate, 8)
 
-	left := Coordinate{gridIndex, lineIndex - 1}
-	topLeft := Coordinate{gridIndex - 1, lineIndex - 1}
-	top := Coordinate{gridIndex - 1, lineIndex}
-	topRight := Coordinate{gridIndex - 1, lineIndex + 1}
-	right := Coordinate{gridIndex, lineIndex + 1}
-	bottomRight := Coordinate{gridIndex - 1, lineIndex + 1}
-	bottom := Coordinate{gridIndex - 1, lineIndex}
-	bottomLeft := Coordinate{gridIndex - 1, lineIndex - 1}
+	left := Coordinate{yIndex, xIndex - 1}
+	topLeft := Coordinate{yIndex - 1, xIndex - 1}
+	top := Coordinate{yIndex - 1, xIndex}
+	topRight := Coordinate{yIndex - 1, xIndex + 1}
+	right := Coordinate{yIndex, xIndex + 1}
+	bottomRight := Coordinate{yIndex + 1, xIndex + 1}
+	bottom := Coordinate{yIndex + 1, xIndex}
+	bottomLeft := Coordinate{yIndex + 1, xIndex - 1}
 
 	positions[0] = left
 	positions[1] = topLeft
@@ -72,35 +82,85 @@ func searchAround(gridIndex, lineIndex int, runeGrid *[][]rune, searchValue rune
 	positions[6] = bottom
 	positions[7] = bottomLeft
 
-	for _, position := range positions {
+	var positionsToSearch []Coordinate
+	if alreadyMovingInDirection != nil {
+		positionsToSearch = append(positionsToSearch, positions[*alreadyMovingInDirection])
+	} else {
+		positionsToSearch = positions
+	}
+
+	total := 0
+	for i, position := range positionsToSearch {
+		var direction int
+		if alreadyMovingInDirection != nil {
+			direction = *alreadyMovingInDirection
+		} else {
+			direction = i
+		}
+
 		if inGrid(position) {
 			charAtPosition := (*runeGrid)[position.X][position.Y]
 			if charAtPosition == searchValue {
+				// fmt.Printf("Found %c in position %s\n", charAtPosition, printDirection(direction))
 				if searchValue == 'S' {
-					return true
+					fmt.Printf("Found XMAS following direction %s\n", printDirection(direction))
+					total++
+					continue
 				}
 
-				return searchAround(position.X, position.Y, runeGrid, *getNextSearch(charAtPosition))
+				total += searchAround(position.X, position.Y, runeGrid, *getNextSearch(charAtPosition), &direction)
 			}
 		}
 	}
 
-	return false
+	return total
+}
+
+func printDirection(i int) string {
+	switch i {
+	case 0:
+		return "left"
+	case 1:
+		return "up left"
+	case 2:
+		return "up"
+	case 3:
+		return "up right"
+	case 4:
+		return "right"
+	case 5:
+		return "down right"
+	case 6:
+		return "down"
+	case 7:
+		return "down left"
+	}
+
+	return "invalid direction"
 }
 
 func inGrid(coord Coordinate) bool {
-	return coord.X >= 0 && coord.X <= maxGridDepth && coord.Y >= 0 && coord.Y <= maxGridWidth
+	inGrid := coord.X >= 0 && coord.X < maxGridHeight && coord.Y >= 0 && coord.Y < maxGridWidth
+	return inGrid
 }
 
 func getNextSearch(r rune) *rune {
 	switch r {
+	case 'X':
+		m := 'M'
+		return &m
 	case 'M':
 		a := 'A'
 		return &a
 	case 'A':
 		s := 'S'
 		return &s
+	case 'S':
+		m := 'M'
+		return &m
 	}
+
+	fmt.Printf("couldn't get next search for character: %c\n", r)
 
 	return nil
 }
