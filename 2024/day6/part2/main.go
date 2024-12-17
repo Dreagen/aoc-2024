001aa36fbf56e4a07130f399a6e9a42efc21ae46
Nick Allen <nick.allen@dataglide.co>
2024-12-17 18:01:40 +0000

    Completed day 6 part 2

--- 2024/day6/part2/main.go
+++ 2024/day6/part2/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 	"time"
 )
 
@@ -15,12 +14,15 @@ const (
 	Right
 )
 
+var gridWidth = 0
+var gridHeight = 0
+
 var shouldLog = false
 var direction = Up
 var total = 0
 var visit = 0
-var verticalBlockers map[int][]int
-var horizontalBlockers map[int][]int
+var verticalBlockers map[int][]bool
+var horizontalBlockers map[int][]bool
 var blockers = []Coordinate{}
 var potentialLoop = []Coordinate{}
 var blockersCausingInfiniteLoop = []CoordinateWithDirection{}
@@ -35,7 +37,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	makeBlockerMaps(grid, blockers)
+	gridWidth = len((*grid)[0])
+	gridHeight = len(*grid)
+
+	makeBlockerMaps(blockers)
 
 	guardPosition := findGuardPosition(grid)
 	loopBlockers := []CoordinateWithDirection{}
@@ -45,28 +50,10 @@ func main() {
 	endTime := time.Since(startTime)
 
 	printInfiniteBlockers(grid, blockersCausingInfiniteLoop)
+
 	printGrid(grid)
 
-	sort.Slice(blockersCausingInfiniteLoop, func(i, j int) bool {
-		if blockersCausingInfiniteLoop[i].X == blockersCausingInfiniteLoop[j].X {
-			return blockersCausingInfiniteLoop[i].Y < blockersCausingInfiniteLoop[j].Y
-		}
-		return blockersCausingInfiniteLoop[i].X < blockersCausingInfiniteLoop[j].X
-	})
-	fmt.Printf("blocks causing infinte loop: %v\n", blockersCausingInfiniteLoop)
 	fmt.Printf("blocks count causing infinte loop: %v\n", len(blockersCausingInfiniteLoop))
-
-	file, err := os.Create("mine.txt")
-	if err != nil {
-		fmt.Printf("Error creating file: %s", err.Error())
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	for _, blocker := range blockersCausingInfiniteLoop {
-		fmt.Fprintf(file, "%v ", blocker)
-	}
-	fmt.Fprintln(file) // Add a newline at the end
 
 	fmt.Printf("total spaces visited: %d\n\n", total)
 	fmt.Printf("executed in %d milliseconds\n", endTime.Milliseconds())
@@ -84,35 +71,21 @@ func walk(guardPosition CoordinateWithDirection, direction int, grid *[][]rune, 
 	}
 	(*grid)[guardPosition.Y][guardPosition.X] = 'X'
 
-	c := Coordinate{
-		X: 14,
-		Y: 111,
-	}
-
 	nextPosition := getNextPosition(guardPosition, direction)
-	if guardPosition.Coordinate == c {
-		fmt.Printf("first: at 4, 1, next position: %v, dirtection: %d\n", nextPosition, direction)
-	}
+
 	if inGrid(nextPosition, grid) == false {
-		if guardPosition.Coordinate == c {
-			fmt.Println("Out of grid")
-		}
 		return
 	}
 
-	if nextPosition.Coordinate == c {
-		fmt.Println("turning on logging")
-		shouldLog = true
-	}
 	if isBlocked(nextPosition, grid) == false {
-		logf("blockers: %v\n", blockers)
 
 		if (*grid)[nextPosition.Y][nextPosition.X] != 'X' && contains(checkedBlockers, nextPosition.Coordinate) == false {
+			addBlockerToMap(nextPosition)
 			if isStuckInLoop(direction, nextPosition, nextPosition, &[]CoordinateWithDirection{}) {
 				blockersCausingInfiniteLoop = append(blockersCausingInfiniteLoop, nextPosition)
 			}
+			removeBlockerFromMap(nextPosition)
 		}
-		shouldLog = false
 
 		walk(nextPosition, direction, grid, loopBlockers)
 	} else {
@@ -123,54 +96,33 @@ func walk(guardPosition CoordinateWithDirection, direction int, grid *[][]rune, 
 			newNextPosition = getNextPosition(guardPosition, direction)
 		}
 		if (*grid)[newNextPosition.Y][newNextPosition.X] != 'X' && contains(checkedBlockers, nextPosition.Coordinate) == false {
+			addBlockerToMap(newNextPosition)
 			if isStuckInLoop(direction, newNextPosition, newNextPosition, &[]CoordinateWithDirection{}) {
 				blockersCausingInfiniteLoop = append(blockersCausingInfiniteLoop, newNextPosition)
 			}
+			removeBlockerFromMap(newNextPosition)
 		}
-		if newNextPosition.Coordinate == c {
-			fmt.Println("turning on logging")
-			shouldLog = true
-		}
-		if guardPosition.Coordinate == c {
-			fmt.Printf("blocked: at 12, 15, next position: %v, dirtection: %d\n", nextPosition, direction)
-		}
+
 		walk(newNextPosition, direction, grid, loopBlockers)
 	}
 }
 
+func addBlockerToMap(c CoordinateWithDirection) {
+	verticalBlockers[c.X][c.Y] = true
+	horizontalBlockers[c.Y][c.X] = true
+}
+
+func removeBlockerFromMap(c CoordinateWithDirection) {
+	verticalBlockers[c.X][c.Y] = false
+	horizontalBlockers[c.Y][c.X] = false
+}
+
 func isStuckInLoop(currentDirection int, initialBlockerCoord, blockerCoord CoordinateWithDirection, loopBlockers *[]CoordinateWithDirection) bool {
-	logf("blocker coord: %v\n", blockerCoord)
 
 	newDirection := getNextDirection(currentDirection)
 
-	if blockerCoord != initialBlockerCoord {
-		switch newDirection {
-		case Up:
-			if newDirection == initialBlockerCoord.Direction && blockerCoord.X+1 == initialBlockerCoord.X && blockerCoord.Y > initialBlockerCoord.Y {
-				logln("Hit inital blocker, found inifinte loop")
-				return true
-			}
-		case Down:
-			if newDirection == initialBlockerCoord.Direction && blockerCoord.X-1 == initialBlockerCoord.X && blockerCoord.Y < initialBlockerCoord.Y {
-				logln("Hit inital blocker, found inifinte loop")
-				return true
-			}
-		case Right:
-			if newDirection == initialBlockerCoord.Direction && blockerCoord.Y+1 == initialBlockerCoord.Y && blockerCoord.X < initialBlockerCoord.X {
-				logln("Hit inital blocker, found inifinte loop")
-				return true
-			}
-		case Left:
-			if newDirection == initialBlockerCoord.Direction && blockerCoord.Y-1 == initialBlockerCoord.Y && blockerCoord.X > initialBlockerCoord.X {
-				logln("Hit inital blocker, found inifinte loop")
-				return true
-			}
-		}
-	}
-
 	for _, blocker := range *loopBlockers {
 		if blocker == blockerCoord {
-			logf("Found blocker in loop blockers, blocker: %v, blocker in list: %v\n", blockerCoord, blocker)
 			return true
 		}
 	}
@@ -178,106 +130,101 @@ func isStuckInLoop(currentDirection int, initialBlockerCoord, blockerCoord Coord
 
 	switch newDirection {
 	case Up:
-		logln("Case Up")
 		currentPosition := blockerCoord.X + 1
 		rowToCheck := verticalBlockers[currentPosition]
 
-		sorted := make([]int, len(rowToCheck))
-		copy(sorted, rowToCheck)
-		sort.Slice(sorted, func(i, j int) bool {
-			return sorted[i] > sorted[j]
-		})
+		for i := len(rowToCheck) - 1; i >= 0; i-- {
+			if i >= blockerCoord.Y {
+				continue
+			}
 
-		for _, blockerPosition := range sorted {
-			logf("Checking blocker position: %d agaisnt currentPosition: %d - needs to be smaller\n", blockerPosition, blockerCoord.Y)
-			if blockerPosition < blockerCoord.Y {
-				innerBlock := CoordinateWithDirection{Coordinate{X: blockerCoord.X + 1, Y: blockerPosition}, Up}
-				logf("blocker position: %d, was smaller than current position: %d\n", blockerPosition, blockerCoord.Y)
+			if rowToCheck[i] == true {
+				innerBlock := CoordinateWithDirection{Coordinate{X: blockerCoord.X + 1, Y: i}, Up}
 				return isStuckInLoop(newDirection, initialBlockerCoord, innerBlock, loopBlockers)
 			}
 		}
 
-		logln("returning false")
 		return false
 	case Down:
-		logln("Case Down")
 		currentPosition := blockerCoord.X - 1
 		rowToCheck := verticalBlockers[currentPosition]
-		for _, blockerPosition := range rowToCheck {
-			logf("Checking blocker position: %d agaisnt currentPosition: %d - needs to be larger\n", blockerPosition, blockerCoord.Y)
-			if blockerPosition > blockerCoord.Y {
-				innerBlock := CoordinateWithDirection{Coordinate{X: blockerCoord.X - 1, Y: blockerPosition}, Down}
-				logf("blocker position: %d, was larger than current position: %d\n", blockerPosition, blockerCoord.Y)
+
+		for i := 0; i < len(rowToCheck); i++ {
+			if i <= blockerCoord.Y {
+				continue
+			}
+
+			if rowToCheck[i] == true {
+				innerBlock := CoordinateWithDirection{Coordinate{X: blockerCoord.X - 1, Y: i}, Down}
 				return isStuckInLoop(newDirection, initialBlockerCoord, innerBlock, loopBlockers)
 			}
 		}
 
-		logln("returning false")
 		return false
 	case Right:
-		logln("Case Right")
 		currentPosition := blockerCoord.Y + 1
 		rowToCheck := horizontalBlockers[currentPosition]
-		for _, blockerPosition := range rowToCheck {
-			logf("Checking blocker position: %d agaisnt currentPosition: %d - needs to be larger\n", blockerPosition, blockerCoord.X)
-			if blockerPosition > blockerCoord.X {
-				innerBlock := CoordinateWithDirection{Coordinate{X: blockerPosition, Y: blockerCoord.Y + 1}, Right}
-				logf("inner block: %d\n", innerBlock)
-				logf("blocker position: %d, was larger than current position: %d\n", blockerPosition, blockerCoord.X)
+
+		for i := 0; i < len(rowToCheck); i++ {
+			if i <= blockerCoord.X {
+				continue
+			}
+
+			if rowToCheck[i] == true {
+				innerBlock := CoordinateWithDirection{Coordinate{X: i, Y: blockerCoord.Y + 1}, Right}
 				return isStuckInLoop(newDirection, initialBlockerCoord, innerBlock, loopBlockers)
 			}
 		}
 
-		logln("returning false")
 		return false
 	case Left:
-		logln("Case Left")
 		currentPosition := blockerCoord.Y - 1
 		rowToCheck := horizontalBlockers[currentPosition]
 
-		sorted := make([]int, len(rowToCheck))
-		copy(sorted, rowToCheck)
-		sort.Slice(sorted, func(i, j int) bool {
-			return sorted[i] > sorted[j]
-		})
+		for i := len(rowToCheck) - 1; i >= 0; i-- {
+			if i >= blockerCoord.X {
+				continue
+			}
 
-		for _, blockerPosition := range sorted {
-			logf("Checking blocker position: %d agaisnt currentPosition: %d - needs to be smaller\n", blockerPosition, blockerCoord.X)
-			if blockerPosition < blockerCoord.X {
-				innerBlock := CoordinateWithDirection{Coordinate{X: blockerPosition, Y: blockerCoord.Y - 1}, Left}
-				logf("inner block: %d\n", innerBlock)
-				logf("blocker position: %d, was smaller than current position: %d\n", blockerPosition, blockerCoord.X)
+			if rowToCheck[i] == true {
+				innerBlock := CoordinateWithDirection{Coordinate{X: i, Y: blockerCoord.Y - 1}, Left}
 				return isStuckInLoop(newDirection, initialBlockerCoord, innerBlock, loopBlockers)
 			}
 		}
 
-		logln("returning false")
 		return false
 	}
 
 	panic("unknown direction")
 }
 
-func makeBlockerMaps(grid *[][]rune, blockers []Coordinate) {
-	verticalBlockers = make(map[int][]int, len((*grid)[0]))
-	horizontalBlockers = make(map[int][]int, len(*grid))
+func makeBlockerMaps(blockers []Coordinate) {
+	verticalBlockers = make(map[int][]bool, gridWidth)
+	horizontalBlockers = make(map[int][]bool, gridHeight)
+
+	for i := 0; i < gridWidth; i++ {
+		verticalBlockers[i] = make([]bool, gridHeight)
+	}
+	for i := 0; i < gridHeight; i++ {
+		horizontalBlockers[i] = make([]bool, gridWidth)
+	}
 
 	for _, blocker := range blockers {
-		verticalBlockers[blocker.X] = append(verticalBlockers[blocker.X], blocker.Y)
-		horizontalBlockers[blocker.Y] = append(horizontalBlockers[blocker.Y], blocker.X)
+		verticalBlockers[blocker.X][blocker.Y] = true
+		horizontalBlockers[blocker.Y][blocker.X] = true
 	}
 
-	for i := range verticalBlockers {
-		sort.Slice(verticalBlockers[i], func(a, b int) bool {
-			return verticalBlockers[i][a] < verticalBlockers[i][b]
-		})
-	}
-
-	for i := range horizontalBlockers {
-		sort.Slice(horizontalBlockers[i], func(a, b int) bool {
-			return horizontalBlockers[i][a] < horizontalBlockers[i][b]
-		})
-	}
+	// for i := range verticalBlockers {
+	// 	sort.Slice(verticalBlockers[i], func(a, b int) bool {
+	// 		return verticalBlockers[i][a] < verticalBlockers[i][b]
+	// 	})
+	// }
+	//
+	// for i := range horizontalBlockers {
+	// 	sort.Slice(horizontalBlockers[i], func(a, b int) bool {
+	// 		return horizontalBlockers[i][a] < horizontalBlockers[i][b]
+	// 	})
+	// }
 
 	// fmt.Printf("Vertical blockers: %v\n", verticalBlockers)
 	// fmt.Printf("Horizontal blockers: %v\n", horizontalBlockers)
@@ -347,7 +294,7 @@ func isBlocked(position CoordinateWithDirection, grid *[][]rune) bool {
 }
 
 func inGrid(coord CoordinateWithDirection, grid *[][]rune) bool {
-	inGrid := coord.X >= 0 && coord.X < len((*grid)[0]) && coord.Y >= 0 && coord.Y < len(*grid)
+	inGrid := coord.X >= 0 && coord.X < gridWidth && coord.Y >= 0 && coord.Y < gridHeight
 	return inGrid
 }
 
@@ -428,16 +375,4 @@ func contains(slice []Coordinate, item Coordinate) bool {
 		}
 	}
 	return false
-}
-
-func logf(format string, a ...any) {
-	if shouldLog {
-		fmt.Printf(format, a...)
-	}
-}
-
-func logln(format string) {
-	if shouldLog {
-		fmt.Println(format)
-	}
 }
